4f7368ee61cb81bd9368a413a59deaaefb6848ba
Tim Brown <timothy.brown2@ocado.com>
2019-10-08 10:33:53 +0100

    Singly-linked list/element removal

---
+++ tasks/Singly-linked_list_element_removal/README.md
@@ -0,0 +1,5 @@
+# Singly-linked_list_element_removal
+
+This is the Racket implementation of the: [Singly-linked_list_element_removal](https://rosettacode.org/wiki/Singly-linked_list_(Element_removal)) task.
+
+Wikipedia reference: ())
---
+++ tasks/Singly-linked_list_element_removal/Singly-linked_list_element_removal.rkt
@@ -0,0 +1,28 @@
+#lang racket/base
+
+;; note that reverse and append are defined in racket/base
+;; and you'd usually use them... but we're gooing to define
+;; remove-at using nothing but pair primitives
+(define (rev l (acc null))
+    (if (null? l)
+        acc
+        (rev (cdr l) (cons (car l) acc))))
+
+(define (++ l m)
+    (if (null? l)
+        m
+        (let recur ((l-rev (rev l)) (acc m))
+          (if (null? l-rev)
+              acc
+              (recur (cdr l-rev) (cons (car l-rev) acc))))))  
+
+(define (remove-at l i (acc null))
+  (cond
+    [(null? l) (rev acc)]
+    [(positive? i) (remove-at (cdr l) (sub1 i) (cons (car l) acc))]
+    [else (++ (rev acc) (cdr l))]))
+
+(displayln (remove-at '(1 2 3) 0))
+(displayln (remove-at '(1 2 3) 1))
+(displayln (remove-at '(1 2 3) 2))
+(displayln (remove-at '(1 2 3) 3))
